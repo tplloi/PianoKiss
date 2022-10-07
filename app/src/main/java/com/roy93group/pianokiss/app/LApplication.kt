088114131b2c061a48500b3ebb4aaafc7ba7076d
loitp <loitp@galaxy.one>
2022-10-07 10:28:11 +0700

    update

---
+++ app/src/main/java/com/roy93group/pianokiss/app/LApplication.kt
@@ -0,0 +1,22 @@
+package com.roy93group.pianokiss.app
+
+import com.loitpcore.annotation.LogTag
+import com.loitpcore.core.base.BaseApplication
+import com.loitpcore.core.common.Constants
+import com.loitpcore.core.utilities.LUIUtil
+import com.loitpcore.data.ActivityData
+
+@LogTag("LApplication")
+class LApplication : BaseApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupApp()
+    }
+
+    private fun setupApp() {
+        ActivityData.instance.type = Constants.TYPE_ACTIVITY_TRANSITION_SLIDE_LEFT
+        LUIUtil.fontForAll = Constants.FONT_PATH
+    }
+}
--- app/src/main/java/com/roy93group/pianokiss/ui/MainActivity.kt
+++ app/src/main/java/com/roy93group/pianokiss/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.roy93group.pianokiss
+package com.roy93group.pianokiss.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roy93group.pianokiss.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
